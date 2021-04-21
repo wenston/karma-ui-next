9fb482df8517af43cda6ccf1ea1794dbe1f561a5
wenyongjiang <329837810@qq.com>
2021-04-21 10:41:31 +0800

    clickOutside

--- src/packages/directives/clickOutside.ts
+++ src/packages/directives/clickOutside.ts
@@ -1,23 +1,64 @@
-import {ref,onMounted,Ref, DirectiveBinding} from 'vue'
-import useEvent from '../use/useEvent'
+//参考：idux-ui https://github.com/IduxFE/idux
+import {Ref, DirectiveBinding, ObjectDirective} from 'vue'
+import {NOOP, isFunction,isObject} from '@vue/shared'
+// import useEvent from '../use/useEvent'
+
+type ClickOutsideHandler = (event: Event) => void
 interface ClickOutsideOptions {
     exclude: Ref<HTMLElement>[],
-    handler:Function
+    handler:ClickOutsideHandler
+}
+interface DocumentHandlerOptions {
+    exclude: HTMLElement[]
+    handler: ClickOutsideHandler
+  }
+export type ClickOutsideBinding = ClickOutsideOptions | ClickOutsideHandler
+const docHandlers = new Map<HTMLElement,DocumentHandlerOptions>()
+function createHandler(
+    el:HTMLElement, 
+    bindingValue:ClickOutsideBinding) {
+    const exclude = [el]
+    let handler:ClickOutsideHandler = NOOP
+    if(isFunction(bindingValue)) {
+        handler = bindingValue
+    }else if(isObject(bindingValue)) {
+        exclude.push(...bindingValue.exclude.map(el=>el.value))
+        handler = bindingValue.handler
+    }
+    docHandlers.set(el, {exclude,handler})
 }
 const clickout = function() {
-    let handlers = []
-    const clickOutside = {
+    const clickOutside:ObjectDirective = {
         mounted(el:HTMLElement,binding:DirectiveBinding) {
-            document.addEventListener('click',(e)=>{
-                const tar = e.target as Node
-                if(tar===el || el.contains(tar)) {return}
-                binding.value()
-            })
+            createHandler(el,binding.value)
         },
         updated(el:HTMLElement,binding:DirectiveBinding) {
-            // console.log(binding)
+            createHandler(el,binding.value)
         },
+        unmounted(el) {
+            docHandlers.delete(el)
+            // if(docHandlers.size===0) {
+            //     document.removeEventListener('click', docListener)
+            // }
+        }
     }
     return clickOutside
 }
+
+const docListener:EventListener = e=> {
+    const tar = e.target as Node
+    docHandlers.forEach(({exclude, handler}, el)=> {
+        const isSelf = tar === el
+        const isContain = el.contains(tar)
+        const isExclude = exclude.length && 
+            exclude.some(_el=>_el===tar || _el.contains(tar))
+        if(isSelf || isContain || isExclude) {return}
+        handler(e)
+    })
+}
+
+
+document.addEventListener('click',docListener)
+
+
 export default clickout()
--- src/packages/use/useEvent.ts
+++ src/packages/use/useEvent.ts
@@ -1,14 +1,13 @@
-import {onMounted, onUnmounted, onUpdated, Ref, getCurrentInstance} from 'vue'
+import {onMounted, onUnmounted, Ref} from 'vue'
 export default function useListener(
     elem:Ref, 
     type:string, 
-    listener:Function) {
-    const ins = getCurrentInstance()
+    listener:EventListener) {
     onMounted(()=>{
         // console.assert(elem.value===null, elem.value)
         // console.log(ins)
         // console.log(elem.value)
-        elem.value.removeEventListener(type, listener)
+        // elem.value.removeEventListener(type, listener)
         elem.value.addEventListener(type, listener)
     })
     // onUpdated(()=>{
